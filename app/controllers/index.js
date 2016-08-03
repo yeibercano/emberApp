20f27acaec6fa322b5c4bf47e88b81c78969df4b
Yeiber Cano <yeibercano@gmail.com>
2016-08-02 19:17:11 -0700

    add controller and model to index

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,15 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	isDisabled: true,
+	emailAddress: '',
 
-  emailAddress: '',
+  isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+  isDisabled: Ember.computed.not('isValid'),
 
-  actualEmailAddress: Ember.computed('emailAddress', function() { 
-    console.log('actualEmailAddress function is called: ', this.get('emailAddress'));
-  }),
+  actions: {
 
-  emailAddressChanged: Ember.observer('emailAddress', function() { 
-    console.log('observer is called', this.get('emailAddress')); 
-  })
+    saveInvitation() {
+      alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
+      this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
+      this.set('emailAddress', '');
+    }
+  }
+
 });
