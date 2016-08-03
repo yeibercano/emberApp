28b3ce479f7a4d9cd54913fe5b99a65245c83c2a
Yeiber Cano <yeibercano@gmail.com>
2016-08-02 21:43:23 -0700

    config env for Firebase

--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,20 @@ module.exports = function(environment) {
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
+
+    firebase: {
+      apiKey: 'xyz',
+      authDomain: 'bookstore-6bee5.firebaseio.com',
+      databaseURL: 'https://bookstore-6bee5.firebaseio.com',
+      storageBucket: 'bookstore-6bee5.appspot.com',
+    },
+
+    // if using ember-cli-content-security-policy
+    contentSecurityPolicy: {
+      'script-src': "'self' 'unsafe-eval' apis.google.com",
+      'frame-src': "'self' https://*.firebaseapp.com",
+      'connect-src': "'self' wss://*.firebaseio.com https://*.googleapis.com"
+    },
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
