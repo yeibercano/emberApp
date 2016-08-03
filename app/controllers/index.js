343e4fad07a59e3a8c2113a3845a8c82c5077656
Yeiber Cano <yeibercano@gmail.com>
2016-08-02 21:52:30 -0700

    refactor to use promise and ES6

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -16,10 +16,10 @@ export default Ember.Controller.extend({
       const email = this.get('emailAddress');
 
       const newInvitation = this.store.createRecord('invitation', { email: email });
-      newInvitation.save();
-
-      this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
-      this.set('emailAddress', '');
+      newInvitation.save().then(() => {
+        this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
+        this.set('emailAddress', '');
+      });
     }
   }
 }); 
