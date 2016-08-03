f261a29b4ac87a0781d2643da7938cc13331d373
Yeiber Cano <yeibercano@gmail.com>
2016-08-02 21:59:58 -0700

    create admin route

--- app/router.js
+++ app/router.js
@@ -9,6 +9,10 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('about');
   this.route('contact');
+
+  this.route('admin', function() {
+    this.route('invitations');
+  });
 });
 
 export default Router;
