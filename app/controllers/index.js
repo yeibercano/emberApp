949a955c5e947b4b7c56ed970d67867c953ab92d
Yeiber Cano <yeibercano@gmail.com>
2016-08-02 21:56:54 -0700

    include id in the response

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -16,8 +16,8 @@ export default Ember.Controller.extend({
       const email = this.get('emailAddress');
 
       const newInvitation = this.store.createRecord('invitation', { email: email });
-      newInvitation.save().then(() => {
-        this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
+      newInvitation.save().then((response) => {
+        this.set('responseMessage', `Thank you! We saved your email address with the following id: ${response.get('id')}`);
         this.set('emailAddress', '');
       });
     }
