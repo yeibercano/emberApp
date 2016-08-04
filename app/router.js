a08a710741ae75853e602a012fb87832d32dc998
Yeiber Cano <yeibercano@gmail.com>
2016-08-03 23:32:23 -0700

    create author and books routes

--- app/router.js
+++ app/router.js
@@ -19,6 +19,8 @@ Router.map(function() {
     this.route('new');
     this.route('edit', { path: '/:library_id/edit' });
   });
+  this.route('authors');
+  this.route('books');
 });
 
 export default Router;
