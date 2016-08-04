ff56715371e1336800fba3a1a60345dfaa9ba8b2
Yeiber Cano <yeibercano@gmail.com>
2016-08-03 22:33:10 -0700

    remove contact

--- app/router.js
+++ app/router.js
@@ -8,7 +8,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('about');
-  this.route('contact');
+  // this.route('contact');
 
   this.route('admin', function() {
     this.route('invitations');
