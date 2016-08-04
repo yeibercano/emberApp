d433baa83299dfd1501a321e7b93e006d8796b14
Yeiber Cano <yeibercano@gmail.com>
2016-08-03 22:32:32 -0700

    add book and relationships

--- app/models/library.js
+++ app/models/library.js
@@ -1,7 +1,14 @@
-import DS from 'ember-data';
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import { hasMany } from 'ember-data/relationships';
+import Ember from 'ember';
 
-export default DS.Model.extend({
-  name: DS.attr('string'),
-  address: DS.attr('string'),
-  phone: DS.attr('string')
+export default Model.extend({
+  name: attr('string'),
+  address: attr('string'),
+  phone: attr('string'),
+
+  books: hasMany('book'),
+
+  isValid: Ember.computed.notEmpty('name'),
 });
